16be075b340e2ad20fb71626b9e10a6c8c7fd866
Nico-Ramz <nicoramz98@gmail.com>
2024-11-02 02:43:02 -0300

    Update DnaValidationController.java

--- src/main/java/org/example/controllers/DnaValidationController.java
+++ src/main/java/org/example/controllers/DnaValidationController.java
@@ -28,36 +28,3 @@ public class DnaValidationController {
         }
     }
 }
-
-/*
-import org.example.dto.dnaSequenceDTO;
-import org.example.entities.DnaValidation;
-import org.example.repositories.DnaValidationRepository;
-import org.example.services.DnaValidationServices;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@CrossOrigin(origins = "*")//Logramos brindar o permitir el acceso o no a nuestra Api a partir de disntos origines
-@RequestMapping(path= "api/v1/DnaValidations") //Es nuestra ruta de acceso
-public class DnaValidationController {
-
-    @Autowired
-    private DnaValidationServices dnaValidationServices;
-
-
-    @PostMapping("/mutant")
-    public ResponseEntity<String> checkMutant(@RequestBody String[] dna) {
-        boolean isMutant = dnaValidationServices.isMutant(dna);
-        if (isMutant) {
-            return ResponseEntity.ok("Mutant DNA detected!");
-        } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Not a mutant.");
-        }
-    }
-
-
-}
-*/
